bacbe4a2c05a246cf7ea0f1ee31d8832d8350089
Zedrick <inedc4zedrickr@gmail.com>
2018-11-19 11:01:31 -0600

    sitio administrador

--- final/admin.py
+++ final/admin.py
@@ -1,3 +1,7 @@
 from django.contrib import admin
-
+from final.models import Profesor, Alumno, Materia, Grado, MateriaAdmin, GradoAdmin
 # Register your models here.
+admin.site.register(Profesor)
+admin.site.register(Alumno)
+admin.site.register(Materia, MateriaAdmin)
+admin.site.register(Grado, GradoAdmin)
